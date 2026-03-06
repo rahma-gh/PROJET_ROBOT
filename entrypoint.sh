63c1bbad80dbea3f8d73308810a85acf50bdd5c6
aminetrimech99 <73242709+aminetrimech99@users.noreply.github.com>
2026-03-06 05:10:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ echo "=== Starting CoppeliaSim (headless mode) ==="
 xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' \
   /opt/coppelia/coppeliaSim \
     -h \
-    -G zmqRemoteApi.rpcPort=23000 \
-    -G zmqRemoteApi.cntPort=23001 \
+    -GzmqRemoteApi.rpcPort=23000 \
+    -GzmqRemoteApi.cntPort=23001 \
     /app/pick_and_place.ttt > coppeliasim.log 2>&1 &
 
 COPPELIA_PID=$!
