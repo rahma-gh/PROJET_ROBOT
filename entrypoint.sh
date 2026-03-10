8e123f28d011d46811952cdb2652d24338f41430
firasgraja <91688222+firasgraja@users.noreply.github.com>
2026-03-10 20:03:29 +0100

    Update4.0 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ echo "=== Starting CoppeliaSim (headless mode) ==="
 xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24' \
 /opt/coppelia/coppeliaSim \
     -h \
-    -s \
+    -s0 \
     -GzmqRemoteApi.rpcPort=23000 \
     -GzmqRemoteApi.cntPort=23001 \
     /app/pick_and_place.ttt \
